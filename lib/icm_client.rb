eeeb9a8b5988b4f37a09416b116048b82084ab6b
Vincent Pizzo <vincent.pizzo@singlewire.com>
2015-03-06 20:46:02 -0600

    Performance tweaks and allow passing of rest-client args

--- lib/icm_client.rb
+++ lib/icm_client.rb
@@ -8,10 +8,9 @@ module ICMClient
   class Client
     # Extend forwardable to delegate to RestClient::Resource's get, post, put, delete, head, and options
     extend Forwardable
+    def_delegators :nested_resource, :get, :post, :put, :delete, :head, :options
 
-    def_delegators :@client, :get, :post, :put, :delete, :head, :options
-
-    attr_reader :url, :client, :access_token
+    attr_reader :base_url, :access_token, :path
 
     # The default limit for list/pagination
     DEFAULT_LIMIT = 100
@@ -19,11 +18,18 @@ module ICMClient
     # Creates a new instance of an InformaCast Mobile REST client
     # Params:
     # +access_token+:: Required argument for interacting with the REST API
-    # +url+:: Optional argument for overriding the default base url. Useful for testing.
-    def initialize(access_token='', url='https://api.icmobile.singlewire.com/api/v1-DEV')
-      @access_token = access_token
-      @url = url
-      @client = RestClient::Resource.new(url, :headers => {:Authorization => "Bearer #{access_token}"})
+    # +client_options+:: Optional argument for supplying additional options to RestClient.
+    # +base_url+:: Optional argument for overriding the default base base_url. Useful for testing.
+    # +path+:: Optional argument for overriding the base path.
+    # +resource+:: Optional argument for overriding the default RestClient.
+    def initialize(access_token, client_options={}, base_url='https://api.icmobile.singlewire.com/api/v1-DEV', path=nil, resource=nil)
+      @access_token = access_token.freeze or raise ArgumentError, 'must pass :access_token'
+      @base_url = base_url.freeze or raise ArgumentError, 'must pass :base_url'
+      @path = path.freeze
+      @resource = resource || RestClient::Resource.new(base_url, {:headers => {
+                                                                         :authorization => "Bearer #{access_token}",
+                                                                         :x_client_version => 'RubyClient 0.0.1'
+                                                                     }}.merge(client_options || {}))
     end
 
     # Provides the magic necessary to chain method calls
@@ -31,7 +37,11 @@ module ICMClient
     #     client = ICMClient::Client.new('<My Access Token>')
     #     puts client.users('<User Id>').devices.get
     def method_missing(symbol, *args)
-      Client.new(@access_token, "#{@url}/#{symbol.to_s.gsub('_', '-')}#{"/#{args.first}" unless args.empty?}")
+      raise ArgumentError, 'only one argument may be provided' if args.length > 1
+      formatted_resource_name = symbol.to_s.gsub('_', '-')
+      resource_id = ("/#{args.first}" unless args.empty?)
+      new_path = "#{@path}/#{formatted_resource_name}#{resource_id}"
+      Client.new(@access_token, nil, @base_url, new_path, @resource)
     end
 
     # Builds a lazy enumerator for paging through resources defined in the InformaCast Mobile API
@@ -43,7 +53,7 @@ module ICMClient
       Enumerator.new do |y|
         while true
           args.first[:params][:start] = next_token if next_token
-          raw_response_str = @client.get(*args)
+          raw_response_str = nested_resource.get(*args)
           response = JSON.parse(raw_response_str)
           resources = response['data']
           resources.each { |resource| y.yield resource }
@@ -52,6 +62,12 @@ module ICMClient
         end
       end
     end
+
+    private
+
+    # Grabs a nested resource based on the path for this client
+    def nested_resource
+      @resource[@path]
+    end
   end
 end
-
